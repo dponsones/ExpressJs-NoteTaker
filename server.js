07253ca6161015166c063aa6e95a02e50bca1b21
Deion Ponsones <dcponson@gmail.com>
2023-04-15 14:12:38 -0400

    initial commit with js updates

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const uuid = require('uuid');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3001;
+
+// Set up middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// HTML routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+// API routes
+app.get('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  const newNote = req.body;
+  newNote.id = uuid.v4();
+  notes.push(newNote);
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+  res.json(newNote);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
